cd5c8b14830a72ad9515d6d28cc53b26d1b79103
Pumprlfl <christian.friedrich.bauer04@gmail.com>
2024-11-04 09:37:38 +0100

    merge

--- Solarsystem/Body.ts
+++ Solarsystem/Body.ts
@@ -1,32 +1,57 @@
-namespace Body {
+namespace SolarSystem {
     export class Body {
         name: string;
+        color: string;
         size: number;
         velocity: number;
-        orbitradius: number;
-        sunpos: number;
-        color: string;
+        orbitRadius: number;
         children: Body[];
 
-        constructor(_name: string, _size: number, _velocity: number, _orbitradius: number, _color: string, _children: Body[]) {
+        constructor(_name: string, _color: string, _size: number, _velocity: number, _orbitRadius: number, _children: Body[]) {
             this.name = _name;
+            this.color = _color;
             this.size = _size;
             this.velocity = _velocity;
-            this.orbitradius = _orbitradius;
-            this.color = _color;
-            this.children = _children
+            this.orbitRadius = _orbitRadius;
+            this.children = _children;
         }
 
-        draw(_timeslice: number, _sunposx?: number,): void {
-            crc2.save();
-            crc2.beginPath();
-            crc2.rotate(this.velocity);
-            crc2.translate(this.orbitradius, 0);
-            crc2.arc(0, 0, this.size, 0, 2 * Math.PI);
+        path: Path2D = new Path2D;
+        absoluteRotation: number = 0;
+
+        update(_timeslice: number): void {
+            let relativeRotation: number = _timeslice * this.velocity;
+            this.absoluteRotation = this.absoluteRotation + relativeRotation;
+
+            crc2.rotate(this.absoluteRotation * Math.PI / 180);
+            crc2.translate(this.orbitRadius, 0);
+
             crc2.fillStyle = this.color;
-            crc2.closePath();
-            crc2.restore();
+            // crc2.beginPath();
+            this.path.ellipse(0, 0, this.size, this.size, 0, 0, Math.PI * 2);
+            // crc2.closePath();
+            crc2.fill(this.path);
+
+            for (let child of this.children) {
+                crc2.save();
+                child.update(_timeslice);
+                crc2.restore();
+            }
+            // console.log(this.absoluteRotation);
         }
 
+        showInfo(_mouseX: number, _mouseY: number): void {
+            const bodyName: HTMLSpanElement = <HTMLSpanElement>document.getElementById("bodyName");
+
+            if (crc2.isPointInPath(this.path, _mouseX, _mouseY)) {
+                bodyName.textContent = this.name;
+            }
+
+            for (let child of this.children) {
+                if (crc2.isPointInPath(child.path, _mouseX, _mouseY)) {
+                    bodyName.textContent = child.name;
+                }
+            }
+        }
     }
 }
